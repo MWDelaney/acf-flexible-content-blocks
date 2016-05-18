543454832da6def93e92f72aa6e1cb76742ce4b7
Michael W. Delaney <michael.delaney@gmail.com>
2016-05-18 16:46:15 -0400

    Update block-title.php

--- templates/blocks/parts/block-title.php
+++ templates/blocks/parts/block-title.php
@@ -1,3 +1,5 @@
-<header>
-    <h2><?php the_sub_field('title'); ?></h2>
-</header>
+<?php if(get_sub_field('title')): ?>
+    <header>
+        <h2><?php the_sub_field('title'); ?></h2>
+    </header>
+<?php endif; ?>
