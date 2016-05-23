2ff089a2a789b4c7ce7bbfa76e8a5d3686ae8547
mwdelaney <michael.delaney@gmail.com>
2016-05-23 15:06:11 -0400

    typo

--- templates/blocks/parts/block-content.php
+++ templates/blocks/parts/block-content.php
@@ -1,4 +1,4 @@
-<?php if(get_sub_field('content'); ?>
+<?php if(get_sub_field('content')): ?>
 	<article class="block-the-content">
     	<?php the_sub_field('content'); ?>
     	<aside><?php cfb_template('blocks/parts/block-cta', get_row_layout()); ?></aside>
