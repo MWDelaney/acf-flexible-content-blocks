fcecfa6729740fc6a7b1fda08578000152deca06
mwdelaney <michael.delaney@gmail.com>
2016-05-23 14:51:01 -0400

    Only show content if field is filled

--- templates/blocks/parts/block-content.php
+++ templates/blocks/parts/block-content.php
@@ -1,4 +1,6 @@
-<article class="block-the-content">
-    <?php the_sub_field('content'); ?>
-    <aside><?php cfb_template('blocks/parts/block-cta', get_row_layout()); ?></aside>
-</article>
+<?php if(get_sub_field('content'); ?>
+	<article class="block-the-content">
+    	<?php the_sub_field('content'); ?>
+    	<aside><?php cfb_template('blocks/parts/block-cta', get_row_layout()); ?></aside>
+	</article>
+<?php endif; ?>
