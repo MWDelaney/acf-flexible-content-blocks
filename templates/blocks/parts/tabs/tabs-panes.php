08acf6e8dbdf50ddc65a7bc0ea3c2f6a0aec82ac
Michael W. Delaney <michael.delaney@gmail.com>
2016-08-02 15:00:38 -0400

    Update tabs-panes.php

--- templates/blocks/parts/tabs/tabs-panes.php
+++ templates/blocks/parts/tabs/tabs-panes.php
@@ -1,4 +1,4 @@
-            <div class="block-addon tab-content-wrapper">
+            <div class="block-addon tab-content">
             <?php $i = 0; while ( have_rows('tabs') ) : the_row(); ?>
 
                 <div role="tabpanel" class="tab-pane fade <?php fcb_is_active($i, 'in'); ?> tab-<?php fcb_the_block_id(get_sub_field('navigation_title')); ?>" id="tab-<?php fcb_the_block_id(get_sub_field('title')); ?>"  style="<?php fcb_block_wrapper_styles(); ?>">
