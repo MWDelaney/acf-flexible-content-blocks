1a1ffadd8da04d6287944e712419882a63bee509
Michael W. Delaney <michael.delaney@gmail.com>
2016-08-02 14:06:25 -0400

    Update tabs-tabs.php

--- templates/blocks/parts/tabs/tabs-tabs.php
+++ templates/blocks/parts/tabs/tabs-tabs.php
@@ -1,7 +1,7 @@
             <div class="block-addon tabs-wrapper">
                 <ul role="tablist" class="nav nav-tabs">
                 <?php $i = 0; while ( have_rows('tabs') ) : the_row(); ?>
-                    <li role="presentation" class="<?php fcb_is_active($i); ?>">
+                    <li role="presentation" class="tab-<?php fcb_the_block_id(get_sub_field('navigation_title')); ?> <?php fcb_is_active($i); ?>">
                         <a href="#tab-<?php fcb_the_block_id(get_sub_field('title')); ?>" aria-controls="<?php get_sub_field('title'); ?>" role="tab" data-toggle="tab">
                             <span>
                                 <?php the_sub_field('navigation_title') ?>
