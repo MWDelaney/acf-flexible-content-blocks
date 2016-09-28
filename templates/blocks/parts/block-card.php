7c67dbe99b22b7035b496452203598cd5fbd5f86
Michael W. Delaney <michael.delaney@gmail.com>
2016-09-28 13:19:23 -0400

    Update block-card.php

--- templates/blocks/parts/block-card.php
+++ templates/blocks/parts/block-card.php
@@ -1,10 +1,6 @@
 <?php
 
-    $content_source = get_sub_field('content_source');
-    $content 		= get_sub_field('content');
     $image 			= get_sub_field('background_image');
-	$link_text 		= get_sub_field('link_text');
-
 
     $classes    = 'card';
 
@@ -15,29 +11,14 @@
 <li class="<?=$classes?>">
 	<div class="card-background" style="<?=$styles?>">
 		<div class="card-inner">
-            <?php cfb_template('blocks/parts/block-media', get_row_layout()); ?>
+			<?php cfb_template('blocks/parts/block-media', get_row_layout()); ?>
 			<h3><?php the_sub_field('title'); ?></h3>
-			<?php
-			$post_object = get_sub_field('link');
-			if( $post_object ): 
-				$post = $post_object;
-				setup_postdata( $post );
-			endif;
-
-			?>
 			<article>
 				<div class="card-content">
-					<?php echo ($content_source == 'excerpt') ? get_the_excerpt() : $content; ?>
+					<?php the_sub_field('content'); ?>
 				</div>
-				<aside>
-					<a class="card-link btn" href="<?php the_permalink(); ?>">
-						<?=$link_text?>
-					</a>
-				</aside>
-
-			    <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+            			<?php cfb_template('blocks/parts/block-cta', get_row_layout()); ?>
 			</article>
 		</div>
 	</div>
-
 </li>
