31c726592a64ee28723408989f3a0f23b2314a17
Michael W. Delaney <michael.delaney@gmail.com>
2016-10-26 13:20:29 -0400

    Update media-image.php

--- templates/blocks/parts/media/media-image.php
+++ templates/blocks/parts/media/media-image.php
@@ -1,9 +1,16 @@
 <?php
-
 $image = get_sub_field('image');
+
+$caption = $image['caption'];
+$title = $image['title'];
+$alt = $image['alt'];
 
 if( !empty($image) ): ?>
 
-	<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+	<img title="<?=$title?>" alt="<?=$alt?>" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+	
+<?php endif; ?>
 
+<?php if( !empty($caption) ): ?>
+<figcaption><?=$caption?></figcaption>
 <?php endif; ?>
