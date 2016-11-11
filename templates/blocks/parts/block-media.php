d55d6f17686852226f1e208240c157f0fc0d9767
Michael W. Delaney <michael.delaney@gmail.com>
2016-11-11 15:05:26 -0500

    Update block-media.php

--- templates/blocks/parts/block-media.php
+++ templates/blocks/parts/block-media.php
@@ -5,7 +5,7 @@
 <?php while ( have_rows('media') ) : the_row(); ?>
 	<figure class="block-addon block-figure block-figure-<?=get_row_layout()?>">
 		<?php cfb_template('blocks/parts/media/media-' . get_row_layout(), $layout); ?>
-		<?php cfb_template('blocks/parts/block-cta', get_row_layout()); ?>
 	</figure>
 <?php endwhile; ?>
 <?php endif; ?>
+<?php cfb_template('blocks/parts/block-cta', get_row_layout()); ?>
